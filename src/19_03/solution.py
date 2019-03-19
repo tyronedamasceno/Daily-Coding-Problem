256d04d3e3cdf641a4fc465a5f0a84d913b0ceb3
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-03-19 20:18:54 -0300

    Solution in one pass: bonus

--- src/19_03/solution.py
+++ src/19_03/solution.py
@@ -35,21 +35,38 @@ def solve(inp_list, k):
     return False
 
 
+def solve_in_one_pass(inp_list, k):
+    if len(inp_list) < 2:
+        return False
+    
+    inp_set = set()
+    for x in inp_list:
+        target = k - x
+        if target in inp_set:
+            return True
+        inp_set.add(x)
+    return False
+
+
 class Tests(unittest.TestCase):
     def test_example(self):
-        ans = solve([10, 15, 3, 7], 17)
+        ans = solve_in_one_pass([10, 15, 3, 7], 17)
+        # ans = solve([10, 15, 3, 7], 17)
         self.assertTrue(ans)
 
     def test_cant_use_the_same_element_twice(self):
         """In this test it should return false because it should not use two \
             times the same element. In the example, k=6 returns False
         """
-        ans = solve([10, 15, 3, 7], 6)
+        ans = solve_in_one_pass([10, 15, 3, 7], 6)
+        # ans = solve([10, 15, 3, 7], 6)
         self.assertFalse(ans)
 
     def test_using_empty_or_unitary_list(self):
-        ans1 = solve([], 10)
-        ans2 = solve([1], 2)
+        ans1 = solve_in_one_pass([], 10)
+        ans2 = solve_in_one_pass([1], 2)
+        # ans1 = solve([], 10)
+        # ans2 = solve([1], 2)
         self.assertFalse(ans1)
         self.assertFalse(ans2)
 
@@ -59,14 +76,18 @@ class Tests(unittest.TestCase):
         """
         with open("large_input_evens.txt") as f:
             l = [int(x) for x in f.readline().split(",")]
-        ans_odd = solve(l, 1001)
-        ans_even = solve(l, l[0] + l[-1])
+        ans_odd = solve_in_one_pass(l, 1001)
+        ans_even = solve_in_one_pass(l, l[0] + l[-1])
+        # ans_odd = solve(l, 1001)
+        # ans_even = solve(l, l[0] + l[-1]
         self.assertFalse(ans_odd)
         self.assertTrue(ans_even)
 
     def test_with_negative_numbers_on_input(self):
         l = [5, -2, 8, 3, 11, -7]
-        ans_ok1 = solve(l, -9)
-        ans_ok2 = solve(l, -4)
+        ans_ok1 = solve_in_one_pass(l, -9)
+        ans_ok2 = solve_in_one_pass(l, -4)
+        # ans_ok1 = solve(l, -9)
+        # ans_ok2 = solve(l, -4)
         self.assertTrue(ans_ok1)
         self.assertTrue(ans_ok2)
