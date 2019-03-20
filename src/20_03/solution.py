2fa4d6e7954c4ce80c6295a6c2c10b040a88790e
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-03-20 14:06:17 -0300

    Solution with no divisions

--- src/20_03/solution.py
+++ src/20_03/solution.py
@@ -20,9 +20,25 @@ def mutiply_items_from_list(inp_list):
     return functools.reduce(lambda x, y: x*y, inp_list)
 
 
+# Time complexity: O(n)
+# def solve(inp_list):
+#     product = mutiply_items_from_list(inp_list)
+#     return [product//x for x in inp_list]
+
 def solve(inp_list):
-    product = mutiply_items_from_list(inp_list)
-    return [product//x for x in inp_list]
+    count_zeros = inp_list.count(0)
+    if count_zeros > 1:
+        return [0] * len(inp_list)
+    elif count_zeros == 1:
+        zero_index = inp_list.index(0)
+        output = [0] * len(inp_list)
+        inp_list.remove(0)
+        product = mutiply_items_from_list(inp_list)
+        output[zero_index] = product
+        return output
+    else:
+        product = mutiply_items_from_list(inp_list)
+        return [product//x for x in inp_list]
 
 
 class Tests(unittest.TestCase):
@@ -34,4 +50,14 @@ class Tests(unittest.TestCase):
     def test_example2(self):
         self.assertEqual(
             solve([3, 2, 1]), [2, 3, 6]
+        )
+    
+    def test_with_one_zero(self):
+        self.assertEqual(
+            solve([0, 1, 2, 3]), [6, 0, 0, 0]
+        )
+    
+    def test_with_more_than_one_zero(self):
+        self.assertEqual(
+            solve([0, 3, 2, 1, 0]), [0, 0, 0, 0, 0]
         )
