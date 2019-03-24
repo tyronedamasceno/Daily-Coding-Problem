1319d22e91b85dbc35c6fe4f1cb87e9e4e5fb925
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-03-23 22:10:46 -0300

    Harder test

--- src/21_03/solution.py
+++ src/21_03/solution.py
@@ -62,9 +62,24 @@ def perform_deserialize(nodes_list):
 
 
 class Tests(unittest.TestCase):
-     def test_example(self):
-         node = Node('root', Node('left', Node('left.left')), Node('right'))
-         self.assertEqual(
-             deserialize(serialize(node)).left.left.val, 'left.left'
-         )
+    def test_example(self):
+        node = Node('root', Node('left', Node('left.left')), Node('right'))
+        self.assertEqual(
+            deserialize(serialize(node)).left.left.val, 'left.left'
+        )
 
+    def test_hard(self):
+        super_left_tree = (
+            Node(1, Node(2, Node(3, Node(4, Node(5, Node(6, Node(7, Node(8,
+            Node(9, Node(10, Node(11, Node(12, Node(13, Node(14, Node(15,
+            Node(16, Node(17, Node(19, Node(18, Node(20, Node(21, Node(22,
+            Node(23, Node(24, Node(25, Node(26, Node(27, Node(28, Node(29,
+            Node(30, Node(31, Node(32, Node(33, Node(34, Node(35, Node(36,
+            Node(37, Node(38, Node(39, Node(40, Node(41, Node(42, Node(43,
+            Node(44, Node(45, Node(46, Node(47, Node(48,
+            Node(49)))))))))))))))))))))))))))))))))))))))))))))))))
+        )
+        str_tree = serialize(super_left_tree)
+        first_null = str_tree.find('@')
+        for x in range(1, 50):
+            self.assertIn(str(x), str_tree[:first_null])
