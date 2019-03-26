280611da3c49cd20ccb331ba5d7c20d048c53ec1
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-03-26 10:42:17 -0300

    Trying to improve with recursion and dynamic programming

--- src/problem_07/solution.py
+++ src/problem_07/solution.py
@@ -10,7 +10,7 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 
 import unittest
 
-def solve(msg): 
+""" def solve(msg): 
     letters = [chr(x) for x in range(96, 123)] 
     numbers = [str(x) for x in range(1, 27)] 
     q = [(0, '')] 
@@ -26,23 +26,44 @@ def solve(msg):
             if p == len(msg) - 2: 
                 xx.append(q[-1][1]) 
     return len(xx) 
+ """
 
+dp = {}
+
+def solve(msg):
+    if dp.get(msg):
+        return dp[msg]
+    if msg == "":
+        return 1
+    if msg[0] == "0":
+        return 0
+    if msg[0] == '1' or (len(msg) > 1 and msg[0] == '2' and msg[1] >= '0' and msg[1] <= '6'):
+        print('dois casos')
+        print(msg)
+        dp[msg] = solve(msg[1:]) + solve(msg[2:])
+        return dp[msg]
+    print('um caso')
+    print(msg)
+    dp[msg] = solve(msg[1:])
+    return dp[msg]
 
 
 class Tests(unittest.TestCase):
-    def test_example(self):
+    """ def test_example(self):
         ans = solve('111')
         self.assertEqual(ans, 3)
-
+ """
     def test_with_1111(self):
         """1111 can be converted in 'aaaa', 'kaa', 'aka', 'aak', 'kk'"""
         ans = solve('1111')
+        print(dp)
         self.assertEqual(ans, 5)
 
-    def test_with_zeroes(self):
+"""     def test_with_zeroes(self):
         ans = solve('10203')
         self.assertEqual(ans, 1)
 
     def test_by_cinthia(self):
         ans = solve('12345678')
         self.assertEqual(ans, 3)
+ """
