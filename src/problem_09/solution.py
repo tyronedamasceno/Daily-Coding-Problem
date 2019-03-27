1282e419d9e3e0d4e74109d9119f642f3de4a69f
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-03-27 14:18:00 -0300

    More tests

--- src/problem_09/solution.py
+++ src/problem_09/solution.py
@@ -12,6 +12,9 @@ Follow-up: Can you do this in O(N) time and constant space?
 
 import unittest
 
+"""
+This solution works but it's has exponential time complexity
+"""
 def solve(inp_list):
     def solve_inner(inp_list, pos, sum):
         if pos >= len(inp_list):
@@ -32,3 +35,21 @@ class Tests(unittest.TestCase):
     def test_example_2(self):
         ans = solve([5, 1, 1, 5])
         self.assertEqual(ans, 10)
+
+    def test_with_negative_numbers(self):
+        ans = solve([-2, 0, -7])
+        self.assertEqual(ans, 0)
+    
+    def test_empty_list(self):
+        ans = solve([])
+        self.assertEqual(ans, 0)
+
+    def test_with_medium_input(self):
+        l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+        ans = solve(l)
+        self.assertEqual(ans, 100)
+
+    def test_with_large_input(self):
+        l = [x for x in range(100)]
+        ans = solve(l)
+        self.assertEqual(ans, 2500)
