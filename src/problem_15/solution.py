55f60bb338908c4fd866cb47db61aa9c6a81f97f
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-08 17:58:14 -0300

    Lot of problems statements

---
+++ src/problem_15/solution.py
@@ -0,0 +1,11 @@
+"""
+Given a stream of elements too large to store in memory, pick a random element
+from the stream with uniform probability.
+"""
+
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def test_example(self):
+        pass
---
+++ src/problem_21/solution.py
@@ -0,0 +1,13 @@
+"""
+Given an array of time intervals (start, end) for classroom lectures
+(possibly overlapping), find the minimum number of rooms required.
+
+For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+"""
+
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def test_example(self):
+        pass
