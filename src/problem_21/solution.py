f04298155857becfac7eb221fb314711749094af
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-09 09:35:07 -0300

    Basic tests and scaffold

--- src/problem_21/solution.py
+++ src/problem_21/solution.py
@@ -7,7 +7,16 @@ For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 
 import unittest
 
+def solve(inp_list=None):
+    if not inp_list:
+        return 0
+
 
 class Tests(unittest.TestCase):
     def test_example(self):
-        pass
+        x = [(30, 75), (0, 50), (60, 150)]
+        self.assertEqual(solve(x), 2)
+
+    def test_empty_lists(self):
+        self.assertEqual(solve(), 0)
+        self.assertEqual(solve([]), 0)
