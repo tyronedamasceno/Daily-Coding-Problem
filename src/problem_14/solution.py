eb7db1a04042ca78b7a49fbd4c301601e9a71eea
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-10 18:45:06 -0300

    In progress

--- src/problem_14/solution.py
+++ src/problem_14/solution.py
@@ -1,12 +1,26 @@
 """
-The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+The area of a circle is defined as πr^2. Estimate π to 3 decimal places using
+a Monte Carlo method.
 
 Hint: The basic equation of a circle is x2 + y2 = r2.
 """
 
 import unittest
+import random
+from math import sqrt
+
+
+def estimate():
+    inside = 0
+    for i in range(10000):
+        x = random.random()
+        y = random.random()
+        k = sqrt(x**2+y**2)
+        if k <= 0.5:
+            inside += 1
+    return inside/1000
 
 
 class Tests(unittest.TestCase):
     def test_example(self):
-        pass
+        self.assertEqual(estimate(), 3.141)
