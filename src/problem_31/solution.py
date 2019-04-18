b916cb94278652643acba9b506ba4dd49426f18d
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-18 16:21:35 -0300

    Using memoization

--- src/problem_31/solution.py
+++ src/problem_31/solution.py
@@ -12,18 +12,25 @@ import unittest
 import string
 
 def solve(s1, s2):
+    dp = {}
     def solve_helper(n, m):
+        if dp.get((n, m)):
+            return dp.get((n, m))
         if n == 0:
+            dp[(n, m)] = m
             return m
         if m == 0:
+            dp[(n, m)] = n
             return n
         if s1[n-1] == s2[m-1]:
-            return solve_helper(n-1, m-1)
-        return 1 + min(
+            dp[(n, m)] = solve_helper(n-1, m-1)
+            return dp[(n, m)]
+        dp[(n, m)] = 1 + min(
             solve_helper(n-1, m-1),
             solve_helper(n, m-1),
             solve_helper(n-1, m)
         )
+        return dp[(n, m)]
     return solve_helper(len(s1), len(s2))
 
 
@@ -35,3 +42,7 @@ class Tests(unittest.TestCase):
     def test_asciiletters(self):
         ans = solve(string.ascii_lowercase, string.ascii_lowercase[::-1])
         self.assertEqual(ans, 26)
+
+    def test_very_large_input(self):
+        ans = solve('a'*500, 'b'*500)
+        self.assertEqual(ans, 500)
