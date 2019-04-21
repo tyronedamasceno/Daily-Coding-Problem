7a5fb8f0f0c716f254c9af93eac36d473844a14b
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-21 19:36:56 -0300

    Solution with bisect.insort

--- src/problem_33/solution.py
+++ src/problem_33/solution.py
@@ -17,10 +17,28 @@ print out:
 2
 """
 
-
 import unittest
+import bisect
+
+def solve(stream):
+    ans = []
+    helper = []
+    for x in stream:
+        bisect.insort(helper, x)
+        h_sz = len(helper)
+        if h_sz % 2 != 0:
+            val = helper[h_sz//2]
+            ans.append(val)
+        else:
+            val = helper[h_sz//2] + helper[(h_sz//2) - 1]
+            ans.append(val/2)
+    return ans
 
 
 class Tests(unittest.TestCase):
     def test_example1(self):
-        pass
+        stream = [2, 1, 5, 7, 2, 0, 5]
+        expected = [2, 1.5, 2, 3.5, 2, 2, 2]
+        ans = solve(stream)
+        for i, x in enumerate(ans):
+            self.assertEqual(expected[i], x)
