49e832343bd4455af5cdafa7e4e985f452f6ca71
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-21 18:53:38 -0300

    Problems statements

---
+++ src/problem_32/solution.py
@@ -0,0 +1,17 @@
+"""
+Suppose you are given a table of currency exchange rates, represented as a
+2D array. Determine whether there is a possible arbitrage: that is, whether
+there is some sequence of trades you can make, starting with some amount A of
+any currency, so that you can end up with some amount greater than A of that
+currency.
+
+There are no transaction costs and you can trade fractional quantities.
+"""
+
+
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def test_example1(self):
+        pass
