e69557d68823b97f59106fff6f3c783f0d090cfc
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-22 14:17:14 -0300

    Solution and tests with a large input

--- src/problem_35/solution.py
+++ src/problem_35/solution.py
@@ -9,11 +9,25 @@ For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should
 become ['R', 'R', 'R', 'G', 'G', 'B', 'B']
 """
 
-
 import unittest
+import random
+
 
 def solve(inp_list):
-    pass
+    i = 0
+    j = 0
+    k = len(inp_list) - 1
+    while j <= k:
+        if inp_list[j] == 'R':
+            inp_list[i], inp_list[j] = inp_list[j], inp_list[i]
+            i += 1
+            j += 1
+        elif inp_list[j] == 'G':
+            j += 1
+        else:
+            inp_list[j], inp_list[k] = inp_list[k], inp_list[j]
+            k -= 1
+    return inp_list    
 
 
 class Tests(unittest.TestCase):
@@ -21,3 +35,8 @@ class Tests(unittest.TestCase):
         given = ['G', 'B', 'R', 'R', 'B', 'R', 'G']
         expected = ['R', 'R', 'R', 'G', 'G', 'B', 'B']
         self.assertEqual(solve(given), expected)
+
+    def test_large_input(self):
+        with open("large_input.txt") as f:
+            given = [x for x in f.readline().split(",")]
+        self.assertEqual(solve(given), sorted(given, reverse=True))
