217b9137a93cf770dfdff5ef433fcf15efd90366
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-23 17:38:58 -0300

    tests

--- src/problem_36/solution.py
+++ src/problem_36/solution.py
@@ -4,14 +4,37 @@ Given the root to a binary search tree, find the second largest node in the tree
 
 """
 My solution is quite simple:
-The second largest node is the larger between the largest node's parent and its left subtree largest.
+The second largest node is the larger between the largest node's parent and its
+left subtree largest.
 
 The solution time complexity is O(logn)
 """
 
 import unittest
 
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def second_largest(root):
+    pass
+
 
 class Tests(unittest.TestCase):
-    def test_example(self):
-        pass
+    def test_largest_with_no_children(self):
+        BST = Node(8, Node(3, Node(1), Node(6, Node(4), Node(7))), Node(10, None, Node(14)))
+        ans = second_largest(BST)
+        self.assertEqual(ans, 10)
+
+    def test_largest_with_left_children(self):
+        BST = Node(8, Node(3, Node(1), Node(6, Node(4), Node(7))), Node(10, None, Node(14, Node(13))))
+        ans = second_largest(BST)
+        self.assertEqual(ans, 13)
+
+    def test_root_with_no_left_subtree(self):
+        BST = Node(8, Node(3, Node(1), Node(6, Node(4), Node(7))))
+        ans = second_largest(BST)
+        self.assertEqual(ans, 7)
