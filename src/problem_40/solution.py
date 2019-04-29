37de8d41c8af12afae91226bf022c5cff14363d1
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-04-29 17:00:08 -0300

    Solution

--- src/problem_40/solution.py
+++ src/problem_40/solution.py
@@ -8,8 +8,24 @@ Do this in O(N) time and O(1) space.
 
 import unittest
 
+def solve(inp_list):
+    bits_sum = [0]*32
+    for x in inp_list:
+        for i in range(32):
+            if x & (1 << i):
+                bits_sum[i] += 1
+    bits_sum = [s%3 for s in bits_sum]
+    ans = 0
+    for i, s in enumerate(bits_sum):
+        if s:
+            ans += 2**i
+    return ans
 
 class Tests(unittest.TestCase):
     def test_example(self):
-        pass
-
+        inp = [6, 1, 3, 3, 3, 6, 6]
+        self.assertEqual(solve(inp), 1)
+    
+    def test_example2(self):
+        inp = [13, 19, 13, 13]
+        self.assertEqual(solve(inp), 19)
