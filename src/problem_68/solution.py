0c6a7d85faacdcdc07782fdb83fe51d7937efd63
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-05-27 14:08:19 -0300

    Fixing comments line length

--- src/problem_68/solution.py
+++ src/problem_68/solution.py
@@ -1,7 +1,12 @@
 """
-On our special chessboard, two bishops attack each other if they share the same diagonal. This includes bishops that have another bishop located between them, i.e. bishops can attack through pieces.
+On our special chessboard, two bishops attack each other if they share the
+same diagonal. This includes bishops that have another bishop located between
+them, i.e. bishops can attack through pieces.
 
-You are given N bishops, represented as (row, column) tuples on a M by M chessboard. Write a function to count the number of pairs of bishops that attack each other. The ordering of the pair doesn't matter: (1, 2) is considered the same as (2, 1).
+You are given N bishops, represented as (row, column) tuples on a M by M
+chessboard. Write a function to count the number of pairs of bishops that
+attack each other. The ordering of the pair doesn't matter: (1, 2) is
+considered the same as (2, 1).
 
 For example, given M = 5 and the list of bishops:
 
@@ -16,7 +21,9 @@ The board would look like this:
 [0 0 b 0 0]
 [0 0 0 0 0]
 [b 0 0 0 0]
-You should return 2, since bishops 1 and 3 attack each other, as well as bishops 3 and 4.
+
+You should return 2, since bishops 1 and 3 attack each other, as well as
+bishops 3 and 4.
 """
 
 import unittest
