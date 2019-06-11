0143563f8db4701b3251c60dc2e44f97707187b2
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-06-11 14:47:45 -0300

    Problem 79 solution

---
+++ src/problem_79/solution.py
@@ -0,0 +1,20 @@
+"""
+Given an array of integers, write a function to determine whether the array
+could become non-decreasing by modifying at most 1 element.
+
+For example, given the array [10, 5, 7], you should return true, since we
+can modify the 10 into a 1 to make the array non-decreasing.
+
+Given the array [10, 5, 1], you should return false, since we can't modify
+any one element to get a non-decreasing array.
+"""
+
+import unittest
+
+class Tests(unittest.TestCase):
+    def test_example1(self):
+        self.assertTrue(solve([10, 5, 7]))
+
+    def test_example2(self):
+        self.assertFalse(solve([10, 5, 1]))
+
